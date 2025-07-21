67f4614489720c3f24322882773c02e8646869c5
sanjayNishad <sanjay12nishad032009@gmail.com>
2025-07-21 18:26:59 +0530

    fix CORS error

--- src/main/java/com/epicpix/epicpix/configs/CORSConfig.java
+++ src/main/java/com/epicpix/epicpix/configs/CORSConfig.java
@@ -13,7 +13,7 @@ public class CORSConfig {
                     @Override
                     public void addCorsMappings(CorsRegistry registry) {
                          registry.addMapping("/**") // allow all endpoints
-                                 .allowedOrigins("http://localhost:5173") // frontend domain
+                                 .allowedOrigins("https://epicpix-frontend.vercel.app") // frontend domain
                                  .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                                  .allowedHeaders("*")
                                  .allowCredentials(true);
